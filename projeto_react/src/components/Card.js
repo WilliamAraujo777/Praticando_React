ec937fd41c824e0d2e846b21fb81e43168f73cb8
WilliamAraujo777 <will.araujo777@gmail.com>
2024-08-22 16:08:17 -0300

    corrigindo css cards

---
+++ projeto_react/src/components/Card.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import Image from './Image';
+
+// Contêiner do card com Flexbox
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  width: 200px;
+  background-color: #fff;
+  padding: 10px;  // Adiciona padding interno ao card
+  margin: 10px;  // Adiciona espaçamento externo ao card
+`;
+
+// Título do card com espaçamento
+const CardTitle = styled.h2`
+  font-size: 18px;
+  margin: 10px 0;
+`;
+
+// Descrição do card com espaçamento
+const CardDescription = styled.p`
+  font-size: 14px;
+  color: #555;
+  margin: 10px 0;  
+`;
+
+// Componente Card
+const Card = ({ title, description, imageUrl }) => {
+  return (
+    <CardContainer>
+      <Image src={imageUrl} alt={title} style={{ width: '100%', height: 'auto', objectFit: 'cover' }} />
+      <CardTitle>{title}</CardTitle>
+      <CardDescription>{description}</CardDescription>
+    </CardContainer>
+  );
+};
+
+export default Card;
---
+++ projeto_react/src/components/Image.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Image = styled.img`
+  width: 150px;
+  height: 150px;
+  margin-bottom: 15px;
+`;
+
+export default Image;
